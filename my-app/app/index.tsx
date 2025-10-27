d3e501777e84302f0080a7a5ec7874a34e908ee4
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2025-10-28 02:21:39 +0800

    initial setup

---
+++ my-app/app/index.tsx
@@ -0,0 +1,12 @@
+import "./globals.css"
+import { Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center w-full">
+            <Text className="text-xl font-bold text-blue-500">
+                xD lol
+            </Text>
+        </View>
+    );
+}
